83dfb052a243c544ac76559f243871ae1a1e29ca
Damian Martinelli <damianmarti@gmail.com>
2025-07-31 04:28:27 -0300

    Challenge 5 upgrade (#3)

--- packages/hardhat/contracts/Challenge5.sol
+++ packages/hardhat/contracts/Challenge5.sol
@@ -5,8 +5,11 @@ import "./INFTFlags.sol";
 
 contract Challenge5 {
     address public nftContract;
-    mapping(address => uint256) public points;
-    uint256 public constant POINTS_TO_MINT = 10;
+    mapping(address => uint8) public points;
+    mapping(address => uint8) public levels;
+    uint8 public constant POINTS_TO_UPGRADE = 10;
+    uint8 public constant MIN_POWER_TO_MINT = 30;
+    uint8 public constant KEY_VALUE = 32;
 
     constructor(address _nftContract) {
         nftContract = _nftContract;
@@ -24,9 +27,17 @@ contract Challenge5 {
         points[tx.origin] += 1;
     }
 
+    function upgradeLevel() public {
+        require(points[tx.origin] >= POINTS_TO_UPGRADE, "Not enough points");
+        points[tx.origin] -= POINTS_TO_UPGRADE;
+        levels[tx.origin] += 1;
+    }
+
     function mintFlag() public {
-        require(points[tx.origin] >= POINTS_TO_MINT, "Not enough points");
-        points[tx.origin] -= POINTS_TO_MINT;
+        require(points[tx.origin] < POINTS_TO_UPGRADE, "Upgrade first");
+        require(points[tx.origin] * levels[tx.origin] >= MIN_POWER_TO_MINT, "Not enough powner to mint");
+        uint8 key = points[tx.origin] << levels[tx.origin];
+        require(key == KEY_VALUE, "Wrong key value");
         INFTFlags(nftContract).mint(tx.origin, 5);
     }
 }
