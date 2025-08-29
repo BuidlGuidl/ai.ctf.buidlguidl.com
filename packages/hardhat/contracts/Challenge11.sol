0881c0e4759ed7d5b309420a62d4cdb3004b4ecd
Damian Martinelli <damianmarti@gmail.com>
2025-08-29 07:35:17 -0300

    Challenge 11 update (#16)

--- packages/hardhat/contracts/Challenge11.sol
+++ packages/hardhat/contracts/Challenge11.sol
@@ -11,10 +11,13 @@ contract Challenge11 {
     }
 
     function mintFlag() public {
-        require(msg.sender != tx.origin, "Not allowed");
+        require(msg.sender != tx.origin, "Same address");
         uint8 senderLast = uint8(abi.encodePacked(msg.sender)[19]);
         uint8 originLast = uint8(abi.encodePacked(tx.origin)[19]);
-        require((senderLast & 0x15) == (originLast & 0x15), "Not allowed");
+        uint8 contractFirst = uint8(abi.encodePacked(address(this))[0]);
+        uint8 senderFirst = uint8(abi.encodePacked(msg.sender)[0]);
+        require((senderLast & 0xF) == (originLast & 0xF), "Wrong sender/origin address");
+        require((senderFirst & 0xF0) == (contractFirst & 0xF0), "Wrong sender/contract address");
         INFTFlags(nftContract).mint(tx.origin, 11);
     }
 }
