addc81639049467cd6927f8126c436acbfb998f0
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2025-12-27 02:23:21 +0530

    up next version (#44)
    * up next version
    
    * wrap header and footer inside suspense

--- packages/nextjs/components/ScaffoldEthApp.tsx
+++ packages/nextjs/components/ScaffoldEthApp.tsx
@@ -1,12 +1,16 @@
-import { Footer } from "./Footer";
+import { Suspense } from "react";
 import { Header } from "./Header";
 
 export const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Suspense>
+        <Header />
+      </Suspense>
       <main className="relative flex flex-col flex-1">{children}</main>
-      <Footer />
+      <Suspense>
+        <Header />
+      </Suspense>
     </div>
   );
 };
