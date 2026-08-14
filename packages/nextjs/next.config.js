e0174d314b1f635b20bb90463e110506b37e7433
Shiv Bhonde <shivbhonde04@gmail.com>
2026-08-14 11:45:59 +0530

    add proxy for backend

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,6 +3,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Since arena session cookie is SameSite=Strict, having a proxy here to keep the cookie sticks to this host.
+  async rewrites() {
+    return [
+      {
+        source: "/arena-api/:path*",
+        destination: "https://arena-api.buidlguidl.com/:path*",
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
